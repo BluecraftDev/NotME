46d825e549014526c6a0583eab725b992d08b0f2
MinhCrafters <59521026+MinhCrafters@users.noreply.github.com>
2021-11-23 10:08:12 +0000

    more fixing

--- commands/info/npmjs.js
+++ commands/info/npmjs.js
@@ -2,6 +2,7 @@ const fetch = require('node-fetch');
 const Discord = require('discord.js');
 const Commando = require('discord.js-commando');
 const Pagination = require('discord-paginationembed');
+const moment = require('moment');
 
 function isIsoDate(str) {
 	if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(str)) return false;
@@ -35,20 +36,30 @@ module.exports = class Command extends Commando.Command {
 
 		const embeds = [];
 
-		for (let i = 1; i <= 5; ++i) {
+		for (let i = 1; i <= 10; ++i) {
 			const embed1 = new Discord.MessageEmbed()
 				.setTitle(response.results[i].package.name)
 
 			for (const [key, value] of Object.entries(response.results[i].package)) {
-				if (typeof value == 'object' && typeof Object.keys(value) !== 'number') {
+				if (typeof value == 'object' && isNaN(Object.keys(value))) {
 					const keys = [];
 					for (const [key1, value1] of Object.entries(value)) {
-						keys.push(`${this.client.functions.toTitleCase(key1)}: ${value1}`);
+						if (value1.toString().startsWith('http')) {
+							keys.push(`[${this.client.functions.toTitleCase(key1).replace('Npm', 'NPM')}](${value1})`);
+						} else {
+							if (typeof value1 == 'object' && isNaN(Object.keys(value1))) {
+								for (const [key2, value2] of Object.entries(value1)) {
+									keys.push(`${value2}`);
+								}
+							} else {
+								keys.push(`${value1}`);
+							}
+						}
 					}
-					embed1.addField(this.client.functions.toTitleCase(key), keys.join('\n'), true);
+					embed1.addField(this.client.functions.toTitleCase(key), keys.join('\n'), false);
 				} else {
 					if (isIsoDate(value)) {
-						embed1.addField(this.client.functions.toTitleCase(key), new Date(value), true);
+						embed1.addField(this.client.functions.toTitleCase(key), `<t:${moment(value).unix()}> (<t:${moment(value).unix()}:R>)`, false);
 					} else {
 						embed1.addField(this.client.functions.toTitleCase(key), value, true);
 					}
@@ -63,8 +74,9 @@ module.exports = class Command extends Commando.Command {
 			.setAuthorizedUsers([message.author.id])
 			.setChannel(message.channel)
 			.setPageIndicator(true)
-			.setPage(3)
+			.setPage(1)
 			.setColor(this.client.config.discord.accentColor)
+			.setFooter(this.client.requestedBy, message.author.displayAvatarURL({ dynamic: true }))
 			.setThumbnail('https://authy.com/wp-content/uploads/npm-logo.png')
 			.build();
 
